a43c8202613dfe7674df352b0d2b2de8917383f3
rdsmatheus <matheus_rds1998@hotmail.com>
2023-01-24 19:43:44 -0300

    algumas melhorias

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,26 @@
-const galeria = document.querySelectorAll("#galeria div img");
-const imgPrincipal = document.querySelector("#principal");
-
-galeria.forEach(function (img, i) {
-  img.addEventListener("click", function (event) {
-    const clicou = event.currentTarget;
+function initGaleria() {
+  const galeria = document.querySelectorAll("#galeria div img");
+  if (galeria.length) {
+    const imgPrincipal = document.querySelector("#principal");
+    // imgPrincipal.classList.add("ativo");
     const texto = document.querySelectorAll(".galeria-conteudo div");
-    imgPrincipal.src = clicou.src;
-    imgPrincipal.classList.add("ativo")
-    galeria.forEach(function (img) {
-      img.classList.remove("borda");
+    texto[0].classList.add("ativo");
+
+    galeria.forEach(function (img, i) {
+      img.addEventListener("click", function (event) {
+        const clicou = event.currentTarget;
+        imgPrincipal.src = clicou.src;
+        imgPrincipal.classList.add("ativo");
+        galeria.forEach(function (img) {
+          img.classList.remove("borda");
+        });
+        galeria[i].classList.add("borda");
+        texto.forEach(function (txt) {
+          txt.classList.remove("ativo");
+        });
+        texto[i].classList.add("ativo");
+      });
     });
-    galeria[i].classList.add("borda");
-    texto.forEach(function (txt) {
-      txt.classList.remove("ativo");
-    });
-    texto[i].classList.add("ativo");
-  });
-});
+  }
+}
+initGaleria();
